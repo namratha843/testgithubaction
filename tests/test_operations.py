fe450c7da7e1646d9269a74668d14ef58c6c561f
Mikasa <nammu.hinata@Mikasa-ka-MacBook-Air.local>
2026-04-13 23:51:11 +0530

    corrected the test imports

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -6,16 +6,16 @@ def test_add():
     assert add(0, 0) == 0
 
 def test_subtract():
-    assert subtract(5, 3) == 2
-    assert subtract(1, 1) == 0
-    assert subtract(0, 0) == 0
+    assert sub(5, 3) == 2
+    assert sub(1, 1) == 0
+    assert sub(0, 0) == 0
 
 def test_multiply():
-    assert multiply(2, 3) == 6
-    assert multiply(-1, 1) == -1
-    assert multiply(0, 0) == 0
+    assert mul(2, 3) == 6
+    assert mul(-1, 1) == -1
+    assert mul(0, 0) == 0
 
 def test_divide():
-    assert divide(6, 3) == 2
-    assert divide(1, 1) == 1
-    assert divide(0, 1) == 0
+    assert div(6, 3) == 2
+    assert div(1, 1) == 1
+    assert div(0, 1) == 0
